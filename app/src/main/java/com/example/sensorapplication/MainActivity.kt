ff63ddb34bc57d3a2b023fa64416604d5f905456
Rakhitha <rakhitharr@live.com>
2019-09-11 11:47:18 +0530

    initialising components

--- app/src/main/java/com/example/sensorapplication/MainActivity.kt
+++ app/src/main/java/com/example/sensorapplication/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.sensorapplication
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+        val resultView = findViewById<TextView>(R.id.resultsView)
+
+        stopButton.isVisible = false
+
+        startButton.setOnClickListener {
+
+        }
+
+        stopButton.setOnClickListener {
+            
+        }
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
