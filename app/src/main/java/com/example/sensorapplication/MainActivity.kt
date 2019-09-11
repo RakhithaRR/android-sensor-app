b2ca5f89f28946d5b67a9d0590e21fc6837928d5
Rakhitha <rakhitharr@live.com>
2019-09-11 11:59:37 +0530

    finalised initial version

--- app/src/main/java/com/example/sensorapplication/MainActivity.kt
+++ app/src/main/java/com/example/sensorapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -21,25 +22,35 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         val startButton = findViewById<Button>(R.id.startButton)
         val stopButton = findViewById<Button>(R.id.stopButton)
-        val resultView = findViewById<TextView>(R.id.resultsView)
+        val resultView = findViewById<TextView>(R.id.resultsTextView)
 
         stopButton.isVisible = false
+        resultView.text = "Offline"
 
         startButton.setOnClickListener {
+            startButton.isVisible = false
+            sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+            stopButton.isVisible = true
 
         }
 
         stopButton.setOnClickListener {
-            
+            stopButton.isVisible = false
+            sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER))
+            resultView.text = "Offline"
+            startButton.isVisible = true
+
         }
 
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onSensorChanged(event: SensorEvent) {
+        resultsTextView.text = "X : \t${event.values[0]} \n\n" +
+                "Y : \t${event.values[1]} \n\n" +
+                "Z : \t${event.values[2]} \n\n"
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        
     }
 }
